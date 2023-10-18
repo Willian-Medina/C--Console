6e1f75dbd9bf0dd848903ad3d572dbde347a405c
Willian Medina <willian-pablomedina@hotmail.com>
2023-10-17 23:15:26 -0300

    alterado para funcao dinamica

--- Projeto-Console-NETFramework/Program.cs
+++ Projeto-Console-NETFramework/Program.cs
@@ -1,2 +1,33 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+
+namespace Program
+{
+    class Nome
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("seja bem vindo, meu caro");
+            string nome = Console.ReadLine();
+
+
+            if(nome =="willian") {
+
+                Console.WriteLine($"Que maravilha ${nome} você é o cara");
+            } else
+            {
+
+                Console.WriteLine($"Vamos continuar ${nome}");
+            }
+            
+
+
+       
+        }
+    }
+}
+
